a5b043c682087f8f26c2181d30d00c65a1ad55ca
zliu9986 <zliu9986@endeavour1.usc.edu>
2021-07-13 17:56:27 -0700

    add script to create 'debug corpu'

---
+++ data/create_debug_corpus.sh
@@ -0,0 +1,14 @@
+source_dir="XLM_pilot_run_21Langs"
+# debug_dir="XLM_pilot_run_21Langs_debug"
+debug_dir="${source_dir}_debug"
+
+cp ${source_dir}/*.test ${debug_dir}/
+cp ${source_dir}/*.valid ${debug_dir}/
+
+N_TRAIN=500000
+for f in ${source_dir}/*.train; do
+    LG="${f%.train}"  # trim extension
+    LG="${LG#${source_dir}/}" # trim path to source_dir
+    echo "Processing ${LG}"
+    head -n $N_TRAIN $f > ${debug_dir}/${LG}.train
+done
